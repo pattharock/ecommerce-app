db92cd0dbaf915b9d7413a98e73a8f308e5f06dc
pattharock <ritvik1609#gmail.com>
2021-01-22 06:07:23 +0800

    MongoDB connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
-import  express from 'express';
-import  dotenv from 'dotenv';
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API is running');
