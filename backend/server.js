14e9598797a4098d44be3ca37d89eee138531385
pattharock <ritvik1609#gmail.com>
2021-01-23 14:37:26 +0800

    backend user authentication route setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -13,16 +14,21 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('API is running');
+  res.status(200)
+  res.json({
+    "message": "API is running"
+  });
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
-
 app.use(errorHandler);
-
+ 
 app.listen(PORT, () => {
   console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.underline.bold);
 })
